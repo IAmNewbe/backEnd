56d9cf3a7bcdee7ab3e8f8810ea22913543f273c
IAmNewbe <ahlulhikam2222@gmail.com>
2023-02-10 10:04:10 +0700

    Modify response status

--- server.js
+++ server.js
@@ -5,8 +5,65 @@ const http = require('http');
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
 
-    response.statusCode = 200;
-    response.end('<h1>Http server is running</h1>');
+    // const {method} = request;
+      // if (method == 'POST'){
+    //     // response.end('<h1>Hello!</h1>');
+    //     let body = [];
+    //     request.on('data', (chunk) => {
+    //         body.push(chunk);
+    //     });
+
+    //     request.on('end', () => {
+    //         body = Buffer.concat(body).toString();
+    //         const {name} = JSON.parse(body);
+    //         response.end(`<h1>Hai, ${name}!</h1>`);
+    //     });
+    // }
+    // if (method == 'GET') {
+    //     response.end('<h1>Hai</h1>');
+    // }
+    const { method, url} = request;
+    
+    if (url === '/'){
+
+        if(method == 'GET'){
+            response.statusCode = 200;
+            response.end('<h1>ini adalah homepage</h1>');
+        }else{
+            response.statusCode = 400;
+            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+        }
+        // response.end('<h1>Halaman ini tidak dapat diakses dengan <any> request</h1>');
+        
+    }else if (url === '/about'){
+        if (method == 'GET'){
+            response.statusCode = 200;
+            response.end('<h1>Halo! ini adalah halaman about</h1>');
+        }else if (method == 'POST'){
+            let body = [];
+
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
+
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+                const {name} = JSON.parse(body);
+                response.statusCode = 200;
+                response.end(`<h1>Halo, ${name} ini adalah halaman about </h1>`);
+            });
+    
+        }else{
+            response.statusCode = 400;
+            response.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
+        }
+        // response.end('<h1>Halaman ini tidak dapat diakses dengan <any> request</h1>');
+
+    }else {
+        response.statusCode = 404;
+        response.end('<h1>Halaman tidak ditemukan!</h1>');
+    }
+    
 };
 
 const server = http.createServer(requestListener);
